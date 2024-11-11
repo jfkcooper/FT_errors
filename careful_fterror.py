083f65326c9fa8717f66a2312e6fd94acd868df3
Joshaniel Cooper <jos.cooper@ess.eu>
2024-11-11 13:43:24 +0100

    Trying to make a reflectivity dataset FT

--- careful_fterror.py
+++ careful_fterror.py
@@ -2,24 +2,50 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from hogben.simulate import SimulateReflectivity
+from refnx.reflect import SLD, ReflectModel
 
-x = np.linspace(0, 1, 1001)
+
+air = SLD(0, name='Air')
+layer1 = SLD(8, name='Layer 1')(thick=100, rough=2)
+substrate = SLD(2.047, name='Substrate')(thick=0, rough=2)
+
+sample_1 = air | layer1 | substrate
+model_1 = ReflectModel(sample_1)
+
+angle_times = [(0.7, 100, 50),
+               (2.0, 100, 200),
+               (5.0, 100, 1000)] # (Angle, Points, Time) - lets be extreme here
+
+# Simulate the experiment.
+data_1 = SimulateReflectivity(model_1, angle_times).simulate()
+length = len(data_1[0])
+#  q, r, dr, counts
+#  use q as x, r as data, and dr**2 as variance
+#  Need to make something like xi for the FT data
+############################
+############################
+
+x = np.linspace(0, 1, length)
 n_xi = 1000
-xi = np.linspace(0.0, 20.0, n_xi)
+xi = np.linspace(0.0, 200.0, n_xi)
 
 # Data with frequencies of 4 and 10 inverse x periods
-data = np.sin(2*np.pi*4*x) + np.sin(2*np.pi*10*x)
+#data = np.sin(2*np.pi*4*x) + np.sin(2*np.pi*10*x)
+data = data_1[1]
 
 # Noise amount, proportional to each component differently
 # 5x the noise on f=10 component
-variance = np.sin(2*np.pi*4*x)**2 + 25*np.sin(2*np.pi*10*x)**2
+#variance = np.sin(2*np.pi*4*x)**2 + 25*np.sin(2*np.pi*10*x)**2
+variance = data_1[2]**2
+
 covariance = np.diag(variance)
 
 #
 # Main calculation of fourier transform with errors
 #
 
-x_xi = 2 *np.pi * x.reshape(1, -1) * xi.reshape(-1, 1)
+x_xi = 2 * np.pi * x.reshape(1, -1) * xi.reshape(-1, 1)
 
 ft_matrix = np.concatenate((np.cos(x_xi), np.sin(x_xi)))
 ft_data = ft_matrix @ data
@@ -58,6 +84,7 @@ plt.figure("Detailed plots")
 
 plt.subplot(2,2,1)
 plt.plot(x, data)
+plt.yscale('log')
 plt.fill_between(x, data - np.sqrt(variance), data+np.sqrt(variance), alpha=0.2)
 # plt.errorbar(x, data, yerr=np.sqrt(variance))
 plt.title("Input data")
@@ -84,12 +111,14 @@ plt.figure("Just main plots")
 
 plt.subplot(2,1,1)
 plt.plot(x, data)
+plt.yscale('log')
 plt.fill_between(x, data - np.sqrt(variance), data+np.sqrt(variance), alpha=0.2)
 plt.title("Input data")
 
 
 plt.subplot(2,1,2)
 plt.plot(xi, ft_power)
+plt.yscale('log')
 plt.fill_between(xi,
                  ft_power - np.sqrt(ft_power_variance),
                  ft_power + np.sqrt(ft_power_variance),
